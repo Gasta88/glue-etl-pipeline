114fd32f254019ee8949a9c135edb1f41d1224c8
Andrea Spoldi <andrea.spoldi@docebo.com>
2021-02-24 10:35:23 +0000

    Bridgecrew fix aws_s3_bucket.dvault-shape-staging-bucket config

--- dvault-infrastructure/dvault-infrastructure.tf
+++ dvault-infrastructure/dvault-infrastructure.tf
@@ -15,6 +15,13 @@ terraform {
 resource "aws_s3_bucket" "dvault-shape-staging-bucket" {
   bucket = "dvault-shape-staging"
   acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   versioning {
     enabled = true
   }
