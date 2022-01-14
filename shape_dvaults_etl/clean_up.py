1dc0af9d8b60170ff345926150063fa2e15a5e20
francesco.gastaldello <francesco.gastaldello@docebo.com>
2022-01-14 10:37:49 +0100

    refactored clean_up script job

--- shape_dvaults_etl/clean_up.py
+++ shape_dvaults_etl/clean_up.py
@@ -13,28 +13,44 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-logger.info("Get run properties for the Glue workflow.")
-args = getResolvedOptions(sys.argv, ["WORKFLOW_NAME", "WORKFLOW_RUN_ID"])
-workflow_name = args["WORKFLOW_NAME"]
-workflow_run_id = args["WORKFLOW_RUN_ID"]
-glue = boto3.client("glue")
-run_properties = glue.get_workflow_run_properties(
-    Name=workflow_name, RunId=workflow_run_id
-)["RunProperties"]
+def get_run_properties():
+    """Return enhanced job properties.
 
-bucket_name = run_properties["landing_bucketname"]
-# DVAULT_FILES = run_properties["dvault_files"].split(";")
+    :return config: dictionary with properties used in flat_dvaults Glue Job."""
+    config = {}
+    logger.info("Get run properties for the Glue workflow.")
+    args = getResolvedOptions(sys.argv, ["WORKFLOW_NAME", "WORKFLOW_RUN_ID"])
+    workflow_name = args["WORKFLOW_NAME"]
+    workflow_run_id = args["WORKFLOW_RUN_ID"]
+    glue = boto3.client("glue")
+    run_properties = glue.get_workflow_run_properties(
+        Name=workflow_name, RunId=workflow_run_id
+    )["RunProperties"]
 
-s3 = boto3.resource("s3", region_name="us-east-1")
-bucket = s3.Bucket(bucket_name)
+    config["BUCKET_NAME"] = run_properties["landing_bucketname"]
+    # DVAULT_FILES = run_properties["dvault_files"].split(";")
+    return config
 
-logger.info("Starting environment clean-up.")
-# for key in DVAULT_FILES:
-#     s3.Object(bucket_name, key).delete()
-# logger.info("data/raw/ prefix clean-up is complete.")
 
-for key in [obj.key for obj in list(bucket.objects.filter(Prefix="data/flat_jsons"))]:
-    s3.Object(bucket_name, key).delete()
-logger.info("data/flat_jsons/ prefix clean-up is complete.")
+def main():
+    """
+    Run main steps in the flat_dvaults Glue Job.
+    """
+    logger.info("Starting environment clean-up.")
+    # for key in DVAULT_FILES:
+    #     s3.Object(bucket_name, key).delete()
+    # logger.info("data/raw/ prefix clean-up is complete.")
+    run_props = get_run_properties()
+    s3 = boto3.resource("s3", region_name="us-east-1")
+    bucket = s3.Bucket(run_props["BUCKET_NAME"])
+    for key in [
+        obj.key for obj in list(bucket.objects.filter(Prefix="data/flat_jsons"))
+    ]:
+        s3.Object(run_props["BUCKET_NAME"], key).delete()
+    logger.info("data/flat_jsons/ prefix clean-up is complete.")
 
-logger.info("Finishing environment clean-up.")
+    logger.info("Finishing environment clean-up.")
+
+
+if __name__ == "__main__":
+    main()
