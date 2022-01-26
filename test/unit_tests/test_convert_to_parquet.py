5aef5b68b18fef6fc14cb2ada6aa73afb8673873
francesco.gastaldello <francesco.gastaldello@docebo.com>
2022-01-26 14:59:42 +0100

    testing remote unit-tests

--- test/unit_tests/test_convert_to_parquet.py
+++ test/unit_tests/test_convert_to_parquet.py
@@ -19,7 +19,7 @@ class ConvertToParquetTestCase(unittest.TestCase):
         warnings.filterwarnings("ignore", category=ResourceWarning)
         warnings.filterwarnings("ignore", category=DeprecationWarning)
         self.spark = SparkSession.builder.master("local").getOrCreate()
-        self.spark.sparkContext.setLogLevel("CRITICAL")
+        self.spark.sparkContext.setLogLevel("FATAL")
         self.dest_folder = os.mkdir(f"{TEST_DATA_DIR}/dest")
         self.ALL_JSONS = [f for f in os.listdir(TEST_DATA_DIR) if os.path.isfile(f)]
         self.table_names = [
